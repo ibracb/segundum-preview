70f26052132d69f2049a40f1f6459c0a1a12e3eb
LucíaOlmosMartínez <lucia.olmosm@um.es>
2025-10-22 16:55:25 +0200

    Avance servicio producto
    Ahora los métodos que no requieren consultas JPQL están implementados.

--- segundumOlmosMartinez/src/main/java/umu/aadd/segundum/servicio/IServicioProductos.java
+++ segundumOlmosMartinez/src/main/java/umu/aadd/segundum/servicio/IServicioProductos.java
@@ -1,8 +1,12 @@
 package umu.aadd.segundum.servicio;
 
 import java.time.Month;
+import java.util.List;
 import java.util.Map;
 
+import repositorio.EntidadNoEncontrada;
+import repositorio.RepositorioException;
+import umu.aadd.segundum.modelo.Categoria;
 import umu.aadd.segundum.modelo.EstadoProducto;
 import umu.aadd.segundum.modelo.Producto;
 
@@ -10,21 +14,23 @@ import umu.aadd.segundum.modelo.Producto;
  * Interfaz del servicio de gestión de productos.
  */
 public interface IServicioProductos {
-	
+
 	/**
 	 * Da de alta un nuevo producto en el sistema.
 	 * 
-	 * @param titulo             Título del producto.
-	 * @param descripcion        Descripción del producto.
-	 * @param precio             Precio del producto.
-	 * @param estado             Estado del producto.
-	 * @param idCategoria        Identificador de la categoría del producto.
-	 * @param envioDisponible    Indica si el envío está disponible para el producto.
-	 * @param idUsuarioVendedor  Identificador del usuario vendedor.
+	 * @param titulo            Título del producto.
+	 * @param descripcion       Descripción del producto.
+	 * @param precio            Precio del producto.
+	 * @param estado            Estado del producto.
+	 * @param idCategoria       Identificador de la categoría del producto.
+	 * @param envioDisponible   Indica si el envío está disponible para el producto.
+	 * @param idUsuarioVendedor Identificador del usuario vendedor.
 	 * @return Identificador único del producto creado.
 	 */
-	public String altaProducto(String titulo, String descripcion, double precio, EstadoProducto estado, String idCategoria, boolean envioDisponible, String idUsuarioVendedor);
-	
+	public String altaProducto(String titulo, String descripcion, double precio, EstadoProducto estado,
+			String idCategoria, boolean envioDisponible, String idUsuarioVendedor)
+			throws RepositorioException, EntidadNoEncontrada;
+
 	/**
 	 * Asigna un lugar de recogida para un producto.
 	 * 
@@ -33,8 +39,9 @@ public interface IServicioProductos {
 	 * @param latitud     Latitud del lugar de recogida.
 	 * @param descripcion Descripción del lugar de recogida.
 	 */
-	public void asignarLugarRecogida(String idProducto, double longitud, double latitud, String descripcion);
-	
+	public void asignarLugarRecogida(String idProducto, double longitud, double latitud, String descripcion)
+			throws RepositorioException, EntidadNoEncontrada;
+
 	/**
 	 * Modifica los datos de un producto existente.
 	 * 
@@ -42,24 +49,37 @@ public interface IServicioProductos {
 	 * @param descripcion Nueva descripción del producto.
 	 * @param precio      Nuevo precio del producto.
 	 */
-	public void modificarDatosProducto(String idProducto, String descripcion, double precio);
-	
+	public void modificarDatosProducto(String idProducto, String descripcion, double precio)
+			throws RepositorioException, EntidadNoEncontrada;
+
 	/**
 	 * Añade una visualización al contador del producto.
 	 * 
 	 * @param idProducto Identificador del producto.
 	 */
-	public void anadirVisualizacion(String idProducto);
-	
+	public void anadirVisualizacion(String idProducto) throws RepositorioException, EntidadNoEncontrada;
+
 	/**
 	 * Recupera el historial de un producto para un mes y año específicos.
 	 * 
 	 * @param mes  Mes del historial.
 	 * @param anio Año del historial.
-	 * @return Mapa con los productos y sus respectivos historiales en forma de cadena.
+	 * @return Mapa con los productos y sus respectivos historiales en forma de
+	 *         cadena.
 	 */
-	public Map<Producto, String> getHistorial(Month mes, int anio);
-	
-	
-	
+	public Map<Producto, String> getHistorial(Month mes, int anio) throws RepositorioException, EntidadNoEncontrada;
+
+	/**
+	 * Recupera los productos a la venta que cumplan con las caracteristicas
+	 * especificadas.
+	 * 
+	 * @param categoria   Categoria del producto.
+	 * @param descripcion Descripcion del producto.
+	 * @param estado      Estado del producto.
+	 * @param precio      Precio del producto.
+	 * @return Lista con los productos ordenados que cumplen con los requisitos
+	 *         especificados.
+	 */
+	public List<Producto> getProductosVenta(Categoria categoria, String descripcion, EstadoProducto estado,
+			double precio) throws RepositorioException, EntidadNoEncontrada;
 }
--- segundumOlmosMartinez/src/main/java/umu/aadd/segundum/servicio/ServicioProductos.java
+++ segundumOlmosMartinez/src/main/java/umu/aadd/segundum/servicio/ServicioProductos.java
@@ -1,44 +1,86 @@
 package umu.aadd.segundum.servicio;
 
 import java.time.Month;
+import java.util.List;
 import java.util.Map;
 
+import repositorio.EntidadNoEncontrada;
+import repositorio.FactoriaRepositorios;
+import repositorio.Repositorio;
+import repositorio.RepositorioException;
+import umu.aadd.segundum.modelo.Categoria;
 import umu.aadd.segundum.modelo.EstadoProducto;
+import umu.aadd.segundum.modelo.LugarRecogida;
 import umu.aadd.segundum.modelo.Producto;
+import umu.aadd.segundum.modelo.Usuario;
 
 /**
  * Implementación del servicio de productos.
  */
 public class ServicioProductos implements IServicioProductos {
 
+	private Repositorio<Categoria, String> repoCategorias = FactoriaRepositorios.getRepositorio(Categoria.class);
+	private Repositorio<Producto, String> repoProductos = FactoriaRepositorios.getRepositorio(Producto.class);
+	private Repositorio<Usuario, String> repoUsuarios = FactoriaRepositorios.getRepositorio(Usuario.class);
+
 	@Override
 	public String altaProducto(String titulo, String descripcion, double precio, EstadoProducto estado,
-			String idCategoria, boolean envioDisponible, String idUsuarioVendedor) {
-		// TODO Auto-generated method stub
+			String idCategoria, boolean envioDisponible, String idUsuarioVendedor)
+			throws RepositorioException, EntidadNoEncontrada {
+
+		Categoria categoria = repoCategorias.getById(idCategoria);
+		Usuario usuario = repoUsuarios.getById(idUsuarioVendedor);
+
+		Producto producto = new Producto(titulo, descripcion, precio, estado, categoria, envioDisponible, usuario);
+		repoProductos.add(producto);
+
+		return producto.getId();
+	}
+
+	@Override
+	public void asignarLugarRecogida(String idProducto, double longitud, double latitud, String descripcion)
+			throws RepositorioException, EntidadNoEncontrada {
+
+		Producto producto = repoProductos.getById(idProducto);
+		LugarRecogida recogida = new LugarRecogida(descripcion, longitud, latitud);
+		producto.setRecogida(recogida);
+		repoProductos.update(producto);
+
+	}
+
+	@Override
+	public void modificarDatosProducto(String idProducto, String descripcion, double precio)
+			throws RepositorioException, EntidadNoEncontrada {
+
+		Producto producto = repoProductos.getById(idProducto);
+		producto.setDescripcion(descripcion);
+		producto.setPrecio(precio);
+		repoProductos.update(producto);
+
+	}
+
+	@Override
+	public void anadirVisualizacion(String idProducto) throws RepositorioException, EntidadNoEncontrada {
+
+		Producto producto = repoProductos.getById(idProducto);
+		producto.setVisualizaciones(producto.getVisualizaciones() + 1);
+		repoProductos.update(producto);
+
+	}
+
+	@Override
+	public Map<Producto, String> getHistorial(Month mes, int anio) throws RepositorioException, EntidadNoEncontrada {
+		
+		/*repositorio = repoProductos.getAll();
+		 * SELECT p FROM repositorio WHERE p.getFechaPublicacion.getMonth.isBefore(mes) AND p.getFechaPublicacion.getAnio.isBefore(anio);
+		 * */
+		
 		return null;
 	}
 
 	@Override
-	public void asignarLugarRecogida(String idProducto, double longitud, double latitud, String descripcion) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void modificarDatosProducto(String idProducto, String descripcion, double precio) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void anadirVisualizacion(String idProducto) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Map<Producto, String> getHistorial(Month mes, int anio) {
-		// TODO Auto-generated method stub
+	public List<Producto> getProductosVenta(Categoria categoria, String descripcion, EstadoProducto estado,
+			double precio) throws RepositorioException, EntidadNoEncontrada {
 		return null;
 	}
 
